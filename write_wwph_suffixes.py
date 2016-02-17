4305a4d623cf5ee09b3a14ce399b2aa33a4fdf2d
Matthias Fripp <mfripp@gmail.com>
2016-02-16 21:22:38 -1000

    added files for aggressive variable fixing and slamming

--- write_wwph_suffixes.py
+++ write_wwph_suffixes.py
@@ -9,3 +9,15 @@ for inputs_dir in inputs_dirs:
                 val=str(max(float(val), 1e-6))
                 f_suffixes.write("    {}:\n".format(var))
                 f_suffixes.write("        CostForRho: {}\n".format(val))
+
+for inputs_dir in inputs_dirs:
+    with open(os.path.join(inputs_dir, "rhos.tsv"), "r") as f_rhos:
+        with open(os.path.join(inputs_dir, "wwph_slam.suffixes"), "w") as f_suffixes:
+            f_suffixes.write("RootNode:\n")
+            for row in f_rhos:
+                var, val = row[:-1].split("\t")
+                val=str(max(float(val), 1e-6))
+                f_suffixes.write("    {}:\n".format(var))
+                f_suffixes.write("        CostForRho: {}\n".format(val))
+                f_suffixes.write("        CanSlamToAnywhere: {}\n".format(True))
+                f_suffixes.write("        SlammingPriority: {}\n".format(1))
